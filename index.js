f93760b663fb964a9d65894c967efdd76265ee93
Himon-SYNCRAFT <d.zawlocki@syncraft.pl>
2016-11-07 22:46:54 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+'use strict'
+
+let express = require('express')
+
+let app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', (request, response) => {
+    response.send('Hello World')
+})
+
+app.listen(port, () => {
+    console.log('App started at port ' + port)
+})
