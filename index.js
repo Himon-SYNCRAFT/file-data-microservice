20fac2f63da2cf398113bbdaba372d49a24e6f0e
Himon-SYNCRAFT <d.zawlocki@syncraft.pl>
2016-11-07 23:27:15 +0100

    File uploading

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 'use strict'
 
 let express = require('express')
+let multer = require('multer')
 
 let app = express()
+let upload = multer({ dest: 'uploads/' })
 const port = process.env.PORT || 3000
 
+app.set('view engine', 'pug')
+
 app.get('/', (request, response) => {
-    response.send('Hello World')
+    response.render('index')
+})
+
+app.post('/', upload.single('file'), (request, response) => {
+    let filesize = request.file.size
+
+    response.setHeader('Content-Type', 'application/json')
+    response.send(JSON.stringify({ size: filesize }))
 })
 
 app.listen(port, () => {
